51be6f90f5c974e57cbb37f6cfbca7c26c266961
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-04-18 09:12:36 +0100

    adding structure - static linking done for mac

---
+++ app/my_test.cpp
@@ -0,0 +1,10 @@
+#include "my_library" // IWYU pragma: keep
+
+#include <iostream>
+
+int main() {
+
+  std::cout << my_library() << std::endl;
+
+  return 0;
+}
---
+++ my_library/includes/common_functions.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef COMMON_FUNCTIONS_HPP
+#define COMMON_FUNCTIONS_HPP
+
+#include <string>
+
+std::string my_library();
+
+double simple_addition(double, double);
+
+double simple_multiplication(double, double);
+
+#endif // COMMON_FUNCTIONS_HPP
